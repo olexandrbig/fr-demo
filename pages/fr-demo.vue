5fa3a2e2a0178e30e523e71f0a7a1e5e64904c3f
Oleksandr Dmukhovskyi <olexandrbig@gmail.com>
2021-08-17 10:16:11 +0300

    test page

---
+++ pages/fr-demo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>
+      Active item details
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  scrollToTop: true,
+  head () {
+    return {
+      title: this.$t('index.title'),
+      meta: [
+        { hid: 'og:site_name', name: 'og:site_name', content: this.$t('index.title') },
+        { hid: 'og:url', property: 'og:url', content: this.$store.state.baseUrl + this.$route.path },
+        { hid: 'name', itemprop: 'name', content: this.$t('index.title') },
+        { hid: 'og:title', property: 'og:title', content: this.$t('index.title') },
+        { hid: 'description', name: 'description', content: this.$t('index.description') },
+        { hid: 'descriptionItem', itemprop: 'description', content: this.$t('index.description') },
+        { hid: 'og:description', property: 'og:description', content: this.$t('index.description') },
+        { hid: 'og:image', property: 'og:image', content: this.$store.state.baseUrl + require('~/assets/pic/logo.png') }
+      ]
+    }
+  }
+}
+</script>
