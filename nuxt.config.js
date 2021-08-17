5f5c06a567020ca27f70e4d3cf1eaa2f280963a1
Oleksandr Dmukhovskyi <olexandrbig@gmail.com>
2021-08-17 10:12:12 +0300

    check structure

--- nuxt.config.js
+++ nuxt.config.js
@@ -85,6 +85,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    publicPath: 'https://olexandrbig.github.io/fr-demo/'
+    publicPath: '/fr-demo/'
   }
 }
